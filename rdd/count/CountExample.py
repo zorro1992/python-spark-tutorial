2bd63bac99123b09a9b50d44192f8c2466acb689
Raghu <skraghunandan11@gmail>
2018-03-31 21:46:25 +0400

    change in spark code

--- rdd/count/CountExample.py
+++ rdd/count/CountExample.py
@@ -2,15 +2,14 @@ from pyspark import SparkContext, SparkConf
 
 if __name__ == "__main__":
     conf = SparkConf().setAppName("count").setMaster("local[*]")
-    sc = SparkContext(conf = conf)
-    
+    sc = SparkContext.getOrCreate()
+
     inputWords = ["spark", "hadoop", "spark", "hive", "pig", "cassandra", "hadoop"]
-    
+
     wordRdd = sc.parallelize(inputWords)
     print("Count: {}".format(wordRdd.count()))
-    
+
     worldCountByValue = wordRdd.countByValue()
     print("CountByValue: ")
     for word, count in worldCountByValue.items():
         print("{} : {}".format(word, count))
-
